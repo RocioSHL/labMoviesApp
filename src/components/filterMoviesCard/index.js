99ee7723c5cf28ebb47ab62e34b96560d028f4da
Rocio Sanchez <20101172@mail.wit.ie>
2022-10-20 15:01:58 +0100

    Added filtering of movie list.

--- src/components/filterMoviesCard/index.js
+++ src/components/filterMoviesCard/index.js
@@ -40,6 +40,8 @@ export default function FilterMoviesCard(props) {
 
   const handleChange = (e, type, value) => {
     e.preventDefault()
+    props.onUserInput(type, value)   // NEW
+  
     // Completed later
   };
   const handleTextChange = e => {
