b1178a7650a99b1dc496fecb7b1245fcf23cf234
Rocio Sanchez <20101172@mail.wit.ie>
2022-10-20 17:28:31 +0100

    Refactor: Template list page; HTTP API calls centralized.

--- src/pages/favouriteMoviesPage.js
+++ src/pages/favouriteMoviesPage.js
@@ -1,7 +1,19 @@
 import React from "react";
+import PageTemplate from "../components/templateMovieListPage";
 
-const favouriteMoviesPage = () => {
-    return <h2>Favourite Movies</h2>
-}
 
-export default favouriteMoviesPage
+const FavouriteMoviesPage = (props) => {
+  const toDo = () => true;
+  // Get movies from local storage.
+  const movies = JSON.parse(localStorage.getItem("favourites")); 
+
+  return (
+    <PageTemplate
+      title="Favourite Movies"
+      movies={movies}
+      selectFavourite={toDo}
+    />
+  );
+};
+
+export default FavouriteMoviesPage;
