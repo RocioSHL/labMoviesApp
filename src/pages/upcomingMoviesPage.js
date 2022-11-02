de59e5e72a08f020e181e9097ae078aa38e0f374
Rocio Sanchez <20101172@mail.wit.ie>
2022-11-02 13:14:04 +0000

    Change upcoming movies page to use caching.

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -3,24 +3,25 @@ import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingmovies } from "../api/tmdb-api";
 import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
 import AddToPlaylistIcon from "../components/cardIcons/addToPlaylist";
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
 const UpcomingPage = (props) => {
-  const [movies, setMovies] = useState([]);
-  const favourites = movies.filter((m) => m.favourite);
-  localStorage.setItem("favourites", JSON.stringify(favourites));
+ 
+  const {  data, error, isLoading, isError }  = useQuery('upcoming',getUpcomingmovies)
 
-  const addToFavourites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favourite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
+  if (isLoading) {
+    return <Spinner />
+  }
 
-  useEffect(() => {
-    getUpcomingmovies().then(movies => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
+  const favourites = movies.filter(m => m.favourite)
+  localStorage.setItem('favourites', JSON.stringify(favourites))
+  const addToFavourites = (movieId) => true 
 
   return (
     <PageTemplate
